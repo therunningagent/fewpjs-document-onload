02943451b9cd3419ee58db29487497861b5c934d
therunningagent <luistjr@gmail.com>
2020-12-21 10:42:48 -0500

    finished

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let paragraph = document.querySelector("#text")
+    paragraph.textContent = "This is really cool!"
+})
+
+console.log("this console.log() fires when index.js loads - bfore DOMContentLoaded is triggered")
